897b98265bf554f2ab28c9f8db1d91508b0698fa
IT21302862 <sanayasamadhi@gmail.com>
2026-01-06 19:59:28 +0530

    update main.py

--- Trash-bot/backend/api/main.py
+++ Trash-bot/backend/api/main.py
@@ -90,7 +90,7 @@ app.add_middleware(
 )
 
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODEL_PATH = os.path.join(BASE_DIR, "saved_models", "1", "trash.keras")
 
 MODEL = tf.keras.models.load_model(MODEL_PATH)
