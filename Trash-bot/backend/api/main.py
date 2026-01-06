cb5d347c21e19afc64bd285482ad30e04aab59cd
IT21302862 <sanayasamadhi@gmail.com>
2026-01-06 20:13:32 +0530

    Update main.py

--- Trash-bot/backend/api/main.py
+++ Trash-bot/backend/api/main.py
@@ -125,3 +125,9 @@ async def predict(file: UploadFile = File(...)):
         "class": predicted_class,
         "confidence": confidence
     }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
